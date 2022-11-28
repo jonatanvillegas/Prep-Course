efb029bae459263a565a03e554160ceb97340e78
jonatan <villegasjoncena7@gmail.com>
2022-11-27 22:36:01 -0600

    optimizacion de funcion esVocal

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -230,9 +230,9 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
+  if (['a','e' ,'i', 'o' ,'u'].includes(letra.toLowerCase())) {
     return 'Es vocal'
-  }else if(letra.length > 0){
+  }else if (letra.length > 0){
     return 'Dato incorrecto'
   }else{
     return 'Dato incorrecto'
